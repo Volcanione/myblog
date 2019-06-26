3e269f5df1b9a426156a927c1ea680fb265ebeaf
Volcanione <1194891819@qq.com>
2019-06-26 23:14:38 +0800

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -9,8 +9,18 @@ const config = {
   multipleStatements: true
 }
 
+const config2 = {
+  host: 'cdb-501hps4o.bj.tencentcdb.com',
+  port: 10158,
+  user: 'root',
+  password: 'hexudong199412',
+  database: 'myblog',
+  multipleStatements: true
+}
+
+
 exports.query = (sql, params, callback) => {
-  const connect = mysql.createConnection(config)
+  const connect = mysql.createConnection(config2)
   connect.connect()
   connect.query(sql, params, (err, data) => {
     callback && callback(err, data)
